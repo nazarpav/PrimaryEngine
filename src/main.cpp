471d2f498815900358c2a36967cdc180fb81889b
NazarPav <hesab.k18@gmail.com>
2022-02-20 04:26:55 +0200

    Change CMakeLists.txt update submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,66 @@
-#include<iostream>
-int main(){
-    std::cout<<"PrimaryEngine\t***\n"<<std::endl;
-    return 0;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+struct FDot {
+	float x;
+	float y;
+};
+struct IDot {
+	int x;
+	int y;
+};
+IDot w_size;
+
+void glfwWindowSizeCallback(GLFWwindow* win, int w, int h) {
+	w_size.x = w;
+	w_size.y = h;
+	glViewport(0, 0, w_size.x, w_size.y);
+}
+void glfwKeyCallback(GLFWwindow* win, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(win, GL_TRUE);
+	}
+}
+int main(void)
+{
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	/* Make the window's context current */
+	glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+	glfwSetKeyCallback(window, glfwKeyCallback);
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL()) {
+		std::cout << "Failed to initialize OpenGL context" << std::endl;
+		return -1;
+	}
+	std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+	glClearColor(0, 1, 0, 1);
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
